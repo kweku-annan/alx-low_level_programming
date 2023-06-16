38a5fa88491e1e53b5dabb93940ed2c5425e869d
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-15 18:24:08 -0700

    Added a C program that prints a piece of text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(void)
 /* This is the main function that prints a message. */
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
