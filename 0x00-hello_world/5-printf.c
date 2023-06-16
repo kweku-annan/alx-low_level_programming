d11070e4ced30754fea583b9447e1acb204c51cb
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-15 18:34:33 -0700

    Added a C program that prints a piece of text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**                                                                                    
-*main - Entry point                                                                    
-*Description: prints "\"with proper grammar, but the outcome is a piece of art,             
-* Return: 0                                                                            
+/**                                                                    
+*main - Entry poin       
+*Description: prints "\"with proper grammar, but the outcome is a piece of art,
+* Return: 0 
 */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
