0d397940b916e4b848fdb8617cf2cf15a6c4a886
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-20 15:28:46 -0700

    Added comments. Cleared white spaces

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,26 @@
 #include <unistd.h>
 #include "main.h"
-
+/**
+*___putchar: Entry point
+* Description: Prints characters
+* Return: give characters
+**/
+/**
+*main: Entry point
+*Description: Uses ___putchar to print characters
+*Return: 0
+**/
 int __putchar(char c)
 {
-  return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
-
-
 int main(void)
 {
-  char message[] = "_putchar\n";
-  int i;
-  for (i=0; message[i] != '\0'; i++)
-    {
-      __putchar(message[i]);
-    }
-  return (0);
+char message[] = "_putchar\n";
+int i;
+for (i=0; message[i] != '\0'; i++)
+{
+__putchar(message[i]);
+}
+return (0);
 }
