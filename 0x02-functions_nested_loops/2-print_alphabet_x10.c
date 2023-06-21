efaad0a17e6ef00a353ff9d0e09e64d9f7a83987
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-21 03:21:04 -0700

    Fixed some errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,10 +4,11 @@
 *print_alphabet_x10 - Prints 10 times the alphabet in lowercase.
 *Returns Void
 **/
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 char c;
-for (i = 0; i <= 0; i++}
+int i;
+for (i = 0; i <= 10; i++}
 {
 for (c = 'a'; c <= 'z'; c++)
 {
