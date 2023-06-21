509f0691d9a2953e180f8bc716749b84e06201a8
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-20 17:58:15 -0700

    Added a script that prints 10 times the alphabet, in lowercase.

---
+++ 0x02-functions_nested_loops/1-alphabets.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+/**
+*__putchar- Prints a character to the standard output
+*@c: The character to be printed
+*
+*Return: On success,  returns the charcater printed. On error, returns -1.
+*/
+int __putchar(char c)
+{
+  return (write(1, &c, 1));
+}
+/**
+*main - Entry point of the program.
+*Description: - Prints the alphabets
+*Return: Always 0 (success)
+**/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+__printchar((char)j);
+}
+__printchar("\n");
+}
+return 0;
+}
