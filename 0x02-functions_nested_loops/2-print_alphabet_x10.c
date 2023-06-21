0cfd926909cf3216c470617f2aa7e5a9265d81dd
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-21 03:12:28 -0700

    Added a script that prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+*print_alphabet_x10 - Prints 10 times the alphabet in lowercase.
+*Returns Void
+**/
+void print_alphabet_x10(void);
+{
+char c;
+for (i = 0; i <= 0; i++}
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+write(1, "\n", 1);
+}
+}
