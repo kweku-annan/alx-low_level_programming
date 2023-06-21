ae4731b69e2fadc4ba2259dfa86bca1193485c9f
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-21 04:13:06 -0700

    Added a short description to _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <ctype.h>
 /**
-*_islower(int c) Checks if an input is a lowercase letter
+*_islower Checks if an input is a lowercase letter
 *@c: The input character
 *Return: 1 if the character is lowercase, 0 if otherwise
 **/
