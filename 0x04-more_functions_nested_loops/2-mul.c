aeb484efd76852471fded75274725ce687381072
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-22 16:15:16 -0700

    Added a script that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+*mul - A function that multiplies two integers
+*@a: The input number
+*@b: The input number
+*Return: Product of a and b
+**/
+int mul(int a, int b)
+{
+return (a * b);
+}
