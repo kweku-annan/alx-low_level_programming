2ee3a88d97e03ea89157c5c84aae0a1546e3071f
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-22 19:05:30 -0700

    fixed error

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,22 @@
 #include "main.h"
+
 /**
 *print_line - Draws a straight line in the terminal
-*@n: Input character
+*@n: The number of times to print the line
 *Return: Nothing
 **/
 void print_line(int n)
 {
 int i = 0;
-while (i <= n)
-{
-if (n == 0)
+if (n <= 0)
 {
 _putchar('\n');
 return;
 }
-else
+while (i < n)
 {
-_putchar("_");
+_putchar('_');
 i++;
-}
 }
 _putchar('\n');
 }
