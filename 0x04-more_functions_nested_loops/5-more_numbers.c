9a6b770938e93f4d9eab089528359c2ef008f097
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-22 18:15:10 -0700

    fixed some errrors

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
-#include <ctype>
+#include <unistd.h>
 /**
 *more_numbers - A function that prints 10 times the numbers, from 0 to 14
-*Returns: Nothing.
+*
+*Returns: Nothing
 **/
 void more_numbers(void)
 {
@@ -11,7 +12,7 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 15; j++)
 {
-_putchar('0' + j);
+_putchar(j);
 }
 _putchar('\n');
 }
