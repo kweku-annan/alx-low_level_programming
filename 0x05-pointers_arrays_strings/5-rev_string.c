56ff8488f0292a7d8b957a819d31249307bc8fd6
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-27 05:54:11 -0700

    Removed a white space from 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 int i, j;
 int length = strlen(s);
 char temp;
- for (i = 0, j = length - 1; i < j; i++, j--)
+for (i = 0, j = length - 1; i < j; i++, j--)
 {
 temp = s[i];
 s[i] = s[j];
