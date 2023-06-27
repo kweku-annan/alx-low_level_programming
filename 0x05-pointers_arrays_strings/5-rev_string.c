7917822decc9618e0ca3721e6247d1918ac6be48
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-27 05:52:20 -0700

    Added a script that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*rev_string - Reverses a string
+*@s: input string
+*Return: Void
+**/
+void rev_string(char *s)
+{
+int i, j;
+int length = strlen(s);
+char temp;
+ for (i = 0, j = length - 1; i < j; i++, j--)
+{
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+}
+}
