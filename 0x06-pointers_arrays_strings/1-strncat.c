da8f4991ff7e10b21fc44db88a17f6a8ad7252f6
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-29 15:25:16 -0700

    Used betty style coding

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,18 +9,18 @@
 **/
 char *_strncat(char *dest, char *src, int n)
 {
-  char *ptr = dest;
-  while (*ptr != '\0')
-    {
-      ptr++;
-    }
-  while (*src != '\0' && n > 0)
-    {
-      *ptr = *src;
-      ptr++;
-      src++;
-      n--;
-    }
-  *ptr = '\0';
-  return (dest);
+char *ptr = dest;
+while (*ptr != '\0')
+{
+ptr++;
+}
+while (*src != '\0' && n > 0)
+{
+*ptr = *src;
+ptr++;
+src++;
+n--;
+}
+*ptr = '\0';
+return (dest);
 }
