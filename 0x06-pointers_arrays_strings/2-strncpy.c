d19059e2289c027b7d355828444fc441c663d0da
kweku-annan <kwekuannan.dev@gmail.com>
2023-06-29 16:31:42 -0700

    Compliance to betty coding style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,7 @@
 *@n: Input int
 *Return: Copy of a string
 **/
-char* _strncpy(char* dest, char* src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 for (i = 0; i < n && src[i] != '\0'; i++)
