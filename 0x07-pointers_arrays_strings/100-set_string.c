ad548d8ab848607d192bb9414d2d2b5ac8fd94d2
kweku-annan <kwekuannan.dev@gmail.com>
2023-07-04 18:51:37 -0700

    Added a script that sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*set_string - Sets the value of a pointer to a char
+*@s: input pointer of pointer
+*@to: input to
+*Return: Void
+**/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
