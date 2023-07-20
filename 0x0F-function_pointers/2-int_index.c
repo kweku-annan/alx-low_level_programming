6ee610f9ba5d99008ca6bd0b5642ce67f40272a2
kweku-annan <kwekuannan.dev@gmail.com>
2023-07-19 22:06:14 -0700

    Added a program that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "functon_pointers.h"
+#include <stdio.h>
+/**
+*int_index - Searches for an integer
+*@array: An input array
+*@size: The size of elements in array
+*@cmp: A pointer to function of one of the 3 in main
+*Return: 0
+**/
+int int_index(int *array, int size, int(*cmp)(int))
+{
+int i;
+if (array == NULL || size <= 0 || cmp == NULL)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+return (-1);
+}
