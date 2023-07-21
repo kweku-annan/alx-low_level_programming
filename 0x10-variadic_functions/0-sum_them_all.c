825b0a4afa90064f840e9016c0f3c8cdbbd6bb17
kweku-annan <kwekuannan.dev@gmail.com>
2023-07-20 22:29:20 -0700

    Added a function that returns the sum of all its parameters.

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+*sum_them_all - Returns the sum of all its parameters
+*@n: Parameters passed into function
+*@...: A variable number
+*Return: An integer
+**/
+int sum_them_all(const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i, sum = 0;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(ap, int);
+}
+va_end(ap);
+return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+ALX projects on Pointers and Functions
