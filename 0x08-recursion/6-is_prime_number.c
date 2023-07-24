0ffb55c606a8d207955a19c0e6c079799ae3232a
kweku-annan <kwekuannan.dev@gmail.com>
2023-07-24 03:29:25 -0700

    Removed loops from 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 /**
-*is_prime_number - Checks if an input integer is prime number
-*@n: Input integer
-*Return: Integer
+*actual_prime - Helper function to check if a number is prime recursively
+*@n: Number to evaluate
+*@i: Iterator
+*Return: 1 if n is prime, 0 if not
+**/
+int actual_prime(int n, int i)
+{
+if (i == 1)
+return (1);
+if (n % i == 0 && i > 0)
+return (0);
+return (actual_prime(n, i - 1));
+}
+/**
+*is_prime_number - Checks if an integer is a prime number or not
+*@n: Number to evaluate
+*Return: 1 if n is a prime number, 0 if not
 **/
 int is_prime_number(int n)
 {
-int i;
 if (n <= 1)
-{
 return (0);
-}
-for (i = 2; i * i <= n; i++)
-{
-if (n % i == 0)
-{
-return (0);
-}
-}
-return (1);
+return (actual_prime(n, n - 1));
 }
