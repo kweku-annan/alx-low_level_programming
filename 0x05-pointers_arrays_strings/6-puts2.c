0eb60c8caf08d0c5c9416bd57782d0846390d937
kweku-annan <kwekuannan.dev@gmail.com>
2024-01-23 15:46:36 +0000

    Correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,18 +8,15 @@
 **/
 void puts2(char *str)
 {
-	int i, str_len = 0;
+	int str_len = 0;
 
 	while (str[str_len] != '\0')
 	{
-		str_len++;
-	}
-	for (i = 0; i <= str_len; i++)
-	{
-		if (i % 2 == 0)
+		if (str_len % 2 == 0)
 		{
-			_putchar(str[i]);
+			_putchar(str[str_len]);
 		}
+		str_len++;
 	}
 	_putchar('\n');
 }
