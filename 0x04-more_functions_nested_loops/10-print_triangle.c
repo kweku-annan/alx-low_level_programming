0564ccb8af9964b0fd6868599d8894aa6803b236
kweku-annan <kwekuannan.dev@gmail.com>
2024-04-08 13:05:07 +0000

    Add 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,10 +7,26 @@
  */
 void print_triangle(int size)
 {
-	if (n <= 0)
+	int i, j;
+
+	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-	
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (j <= size - i)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
 }
