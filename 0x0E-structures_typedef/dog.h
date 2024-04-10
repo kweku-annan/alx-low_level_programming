6225215c82b1439e3007ca4f3c6f60521cdf75c6
kweku-annan <kwekuannan.dev@gmail.com>
2024-04-10 01:33:39 +0000

    Add new stuff

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Data for dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: owner of the dog
+ **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
