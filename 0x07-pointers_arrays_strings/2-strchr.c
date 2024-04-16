7daba13daafa2c751d18d2a4fcb539b14888405e
kweku-annan <kwekuannan.dev@gmail.com>
2024-04-16 22:01:34 +0000

    Add some changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,19 @@
 **/
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
-{
-if (*s == c)
-{
-return (s);
-}
-s++;
-}
-if (c == '\0')
-{
-return (s);
-}
-return (NULL);
+	if (c == '\0' || c == NULL)
+		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
 }
